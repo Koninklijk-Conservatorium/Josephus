c3fbef6f236144d49e732880d0e2043062230d16
Artur J. Dobija <arturdobija@gmail.com>
2026-07-14 16:48:30 +0200

    Prototyping MEI tools. They might be moved to an external package.

---
+++ src/utils/mei-tools/mei-spec.ts
@@ -0,0 +1,15 @@
+// const fs = require('node:fs');
+// const { DOMParser, XMLSerializer } = require('@xmldom/xmldom');
+// var xpath = require('xpath');
+
+// const parser = new DOMParser();
+// const parse = parser.parseFromString;
+
+// const serializer = new XMLSerializer();
+// const serialize = serializer.serializeToString;
+
+// const meiFile = './src/assets/mei-basic.rng';
+// const meiContent: string = fs.readFileSync(meiFile, 'utf8');
+// const meiXML = parse(meiContent, 'text/xml');
+
+// // const definitions = xpath.select('//define', meiXML);
