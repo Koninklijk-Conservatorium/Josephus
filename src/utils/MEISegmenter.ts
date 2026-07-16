93300b7657e9813dce93698c4d2755b02ab7da2b
Artur J. Dobija <arturdobija@gmail.com>
2026-07-16 13:03:50 +0200

    Implemented segmenter.

--- src/utils/MEISegmenter.ts
+++ src/utils/MEISegmenter.ts
@@ -1,8 +1,95 @@
-// abstract class MEISegmenter extends MEITransformer {
-//   pitches(mei: MEIDocument) { return mei } // [mei]
-//   fermata(mei: MEIDocument) { return mei } // [mei]
-//   rests(mei: MEIDocument) { return mei } // [mei]
-// }
+import { MEITransform } from "./MEITransform";
+
+/*
+  NOTE on segmenting MEI files.
+  MEI files have their own api for retrieving file selections
+  "Enhancing Music Addressability for MEI"
+
+  https://github.com/music-addressability/ema-for-mei
+
+  This is however and old codebase.
+
+  For now, I'll use tools available in Verovio:
+  * simplified 'selection' API for retrieving measures
+  * tracking events "sounding" in selection using Timemap.
+*/
+
+abstract class MEISegmenter extends MEITransform {
+
+  abstract segmentate(mei: MEIDocument): SelectionMEI[]
+
+  applySelection(mei:MEIDocument, selection: SelectionMEI): MEIDocument {
+
+    /*
+      For now, apply only measureRange by filtering measures.
+      (see: FutureMilestoneSpec)
+    */
+    if (!selection.measureRange.start && !selection.measureRange.end) {
+      console.warn('Segmenter: segmenting on measureRange string not yet supported.')
+      return mei
+    }
+    const getN = (measureId:string) => this.select(`//mei:measure[@xml:id=${measureId}]/@n`, mei)[0]
+    const start = Number(getN(selection.measureRange.start!))
+    const end = Number(getN(selection.measureRange.end!))
+
+    const copy = mei.cloneNode(true) as MEIDocument
+    this.select(`//mei:measure[@n<'${start}']`, copy).forEach(measure => {
+      measure.parentNode?.removeChild(measure)
+    })
+    this.select(`//mei:measure[@n>'${end}']`, copy).forEach(measure => {
+      measure.parentNode?.removeChild(measure)
+    })
+
+    // TO DO like this:
+    // const measures = this.select(`//mei:measure[@n<=${start} and @n>=${end}]`)
+
+    return copy
+  }
+
+}
+
+
+export class MEIBracketsSegmenter extends MEISegmenter {
+  constructor() { super() }
+
+  segmentate(mei: MEIDocument): SelectionMEI[] {
+
+    const dummyRange: SelectionMEI = {
+      measureRange: {
+        measureRange: "start-end"
+      }
+    }
+
+    const segments = this.select("//mei:bracketSpan", mei).map(bracket => {
+
+      const startID = bracket.getAttribute('startid')?.slice(1) /* Needs to remove '#'. */
+      const endID = bracket.getAttribute('endid')?.slice(1)
+      if (!startID || !endID) return dummyRange
+
+      const startMeasureID = this.select(`//*[@xml:id='${startID}']/ancestor-or-self::mei:measure`, mei)?.[0].getAttribute("xml:id") // beware: null -> 0.
+      const endMeasureID = this.select(`//*[@xml:id='${endID}']/ancestor-or-self::mei:measure`, mei)?.[0].getAttribute("xml:id")
+      if (!startMeasureID || !endMeasureID) return dummyRange
+
+      return {
+        eventRange: {
+          start: startID,
+          end: endID
+        },
+        measureRange: {
+          start: startMeasureID,
+          end: endMeasureID
+        }
+      }
+    })
+
+    return segments
+  }
+
+  transform(mei: MEIDocument): MEIDocument {
+    mei.selections = this.segmentate(mei)
+    return mei
+  }
+}
 
 
 
